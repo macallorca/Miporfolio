db5b30e2fe3a7759a340a949f298e9ffb180f910
llorcamacarena91@gmail.com <maca.izalles@hotmail.com>
2025-09-10 16:31:44 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './BarradeNavegacion';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
+    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
